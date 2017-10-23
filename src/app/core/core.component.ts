8e7d22fbbfbc3f45959b121908199206e759b311
rmroot <mroot@intertech.com>
2017-10-23 11:35:25 -0500

    failed attempts

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -29,25 +29,25 @@ export class CoreComponent implements OnInit {
 
   ngOnInit() {
     //set up listener for update
-    this.electronService.ipcRenderer.once('available', (event, arg) => {
-      console.log('update available: ' + arg);
-      if (arg == true) {
-        this.showUpdateModal();
-      }
-    })
+    // this.electronService.ipcRenderer.once('available', (event, arg) => {
+    //   console.log('update available: ' + arg);
+    //   if (arg == true) {
+    //     this.showUpdateModal();
+    //   }
+    // })
 
-    //send signal to main.js to check for update
-    this.electronService.ipcRenderer.send('ready', null);
+    // //send signal to main.js to check for update
+    // this.electronService.ipcRenderer.send('ready', null);
 
-    this.importExportService.toggleDownload.subscribe((val) => {
-      if (val == true) {
-        this.downloadData();
-      }
-    })
+    // this.importExportService.toggleDownload.subscribe((val) => {
+    //   if (val == true) {
+    //     this.downloadData();
+    //   }
+    // })
 
-    if (this.electronService.process.platform == 'win32') {
-      this.showScreenshot = false;
-    }
+    // if (this.electronService.process.platform == 'win32') {
+    //   this.showScreenshot = false;
+    // }
   }
 
 
